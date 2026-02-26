7e399a14548866147bd6187cb351f3790d139490
Jhantu Samui <jhantusamui.ikrab@gmail.com>
2026-02-26 18:06:02 +0530

    url change

--- Server/helper/token.js
+++ Server/helper/token.js
@@ -5,18 +5,18 @@ const generateToken = (id) => {
 };
 
 const setToken = (res, token) => {
-  // res.cookie("videoToken", token, {
-  //   httpOnly: true,
-  //   secure: true, // true in production (HTTPS)
-  //   sameSite: "None",
-  //   maxAge: 30 * 24 * 60 * 60 * 1000,
-  // });
   res.cookie("videoToken", token, {
     httpOnly: true,
-    secure: false, // true in production (HTTPS)
-    sameSite: "Strict",
+    secure: true, // true in production (HTTPS)
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
+  // res.cookie("videoToken", token, {
+  //   httpOnly: true,
+  //   secure: false, // true in production (HTTPS)
+  //   sameSite: "Strict",
+  //   maxAge: 30 * 24 * 60 * 60 * 1000,
+  // });
 };
 
 export { generateToken, setToken };
